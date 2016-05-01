4c5b71e113fed12f2149a953689382f89d245796
kingair55 <mikielsen@yahoo.com>
2016-05-01 11:54:48 +0100

    added countdown and tournament match stage info. Also updated UI too

--- js/MainController.js
+++ js/MainController.js
@@ -21,18 +21,51 @@ app.controller('ListTournamentsController', function($scope, $location){
 	};
 });
 
-app.controller('ViewTournamentController', function($scope, $routeParams, $location){
+app.controller('ViewTournamentController', function($scope, $routeParams, $location, $interval){
 	$scope.tournamentName = $routeParams.tournamentName;
 	$scope.backToListTournament = function (){
 		$location.path('/');
 	};
 
+    var intervalPromise;
     //$scope.matches=['Team Secret vs Evil Geniuses','Fnatic v Mineski','OG vs Liquid', 'EHOME vs Vici Gaming'];
-    var obj1 = { matchId: 1111, team1:{name:'Team Secret', icon:'https://pbs.twimg.com/profile_images/587609238479986688/OnTi5wQI.png'}, team2:{name:'Team Secret', icon:'https://pbs.twimg.com/profile_images/587609238479986688/OnTi5wQI.png'} };
-    var obj2 = { matchId: 2222, team1:{name:'Team Secret', icon:'https://pbs.twimg.com/profile_images/587609238479986688/OnTi5wQI.png'}, team2:{name:'Team Secret', icon:'https://pbs.twimg.com/profile_images/587609238479986688/OnTi5wQI.png'} };
-    var obj3 = { matchId: 3333, team1:{name:'Team Secret', icon:'https://pbs.twimg.com/profile_images/587609238479986688/OnTi5wQI.png'}, team2:{name:'Team Secret', icon:'https://pbs.twimg.com/profile_images/587609238479986688/OnTi5wQI.png'} };
-    var obj4 = { matchId: 4444, team1:{name:'Team Secret', icon:'https://pbs.twimg.com/profile_images/587609238479986688/OnTi5wQI.png'}, team2:{name:'Team Secret', icon:'https://pbs.twimg.com/profile_images/587609238479986688/OnTi5wQI.png'} };
+    var obj1 = { matchId: 1111, team1:{name:'Team Liquid', icon:'https://pbs.twimg.com/profile_images/587609238479986688/OnTi5wQI.png'}, team2:{name:'Team Secret', icon:'https://pbs.twimg.com/profile_images/587609238479986688/OnTi5wQI.png'}, schedule:(new Date(2016, 4, 5)).getTime(), stage:'Upper Bracket - Main event' };
+    var obj2 = { matchId: 2222, team1:{name:'Team LoooooooooooongNameeeeeeeeeeeeeeeee', icon:'https://pbs.twimg.com/profile_images/587609238479986688/OnTi5wQI.png'}, team2:{name:'Team Secret', icon:'https://pbs.twimg.com/profile_images/587609238479986688/OnTi5wQI.png'}, schedule:(new Date(2016, 4, 10)).getTime(), stage:'Lower Bracket - Main event' };
+    var obj3 = { matchId: 3333, team1:{name:'Team Secret', icon:'https://pbs.twimg.com/profile_images/587609238479986688/OnTi5wQI.png'}, team2:{name:'Team Secret', icon:'https://pbs.twimg.com/profile_images/587609238479986688/OnTi5wQI.png'}, schedule:(new Date(2016, 4, 15)).getTime(), stage:'Group stage' };
+    var obj4 = { matchId: 4444, team1:{name:'Team Secret', icon:'https://pbs.twimg.com/profile_images/587609238479986688/OnTi5wQI.png'}, team2:{name:'Team Secret', icon:'https://pbs.twimg.com/profile_images/587609238479986688/OnTi5wQI.png'}, schedule:(new Date(2016, 4, 22)).getTime(), stage:'Qualifiers - 2nd round' };
     $scope.matches = [obj1, obj2, obj3, obj4];
+
+    $scope.countdownList =[{countdown:"blah"}, {countdown:"blah"}, {countdown:"blah"}, {countdown:"blah"}];
+    $scope.updateTime = function(){
+        for(var x = 0; x < $scope.matches.length; x++)
+        {
+            var timeDifference = ($scope.matches[x].schedule - Date.now()) / 1000;
+            debugger;
+            var rawDays = timeDifference / (60 * 60 * 24);
+            var daysLeft = rawDays >= 1 ? Math.floor(rawDays) : 0;
+            var rawHours = daysLeft > 0 ? (rawDays - daysLeft) * 24 : rawDays * 24;
+            var hoursLeft = rawHours >= 1 ? Math.floor(rawHours) : 0;
+            var rawMinutes = hoursLeft > 0 ? (rawHours - hoursLeft) * 60 : rawHours * 60;
+            var minutesLeft = rawMinutes >= 1 ? Math.floor(rawMinutes) : 0;
+            var rawSeconds = minutesLeft > 0 ? (rawMinutes - minutesLeft) * 60 : rawMinutes * 60;
+            var secondsLeft = Math.floor(rawSeconds);
+
+            if(daysLeft < 1 && hoursLeft < 1 && minutesLeft < 1 && secondsLeft < 1)
+            {
+                $interval.cancel(intervalPromise);
+                $scope.countdownList[x].countdown = (daysLeft == 0 ? "" : daysLeft + 'D ')  + (hoursLeft == 0 ? "" : hoursLeft + 'H ') + (minutesLeft == 0 ? "" : minutesLeft + 'M ') + (secondsLeft == 0 ? "" : secondsLeft + 'S');
+                return;
+            }
+
+            $scope.countdownList[x].countdown = (daysLeft == 0 ? "" : daysLeft + 'D ')  + (hoursLeft == 0 ? "" : hoursLeft + 'H ') + (minutesLeft == 0 ? "" : minutesLeft + 'M ') + (secondsLeft == 0 ? "" : secondsLeft + 'S');
+        }
+    }
+
+    var countDownLimit = Math.round(($scope.matches[0].schedule - Date.now()) / 1000);
+    debugger;
+
+    intervalPromise = $interval($scope.updateTime, 1000, countDownLimit);
+
     $scope.viewMatch = function (matchObj){
         $location.path('ViewMatch' + '/' + $scope.tournamentName + '/' + matchObj.matchId);
     };
@@ -47,3 +80,8 @@ app.controller('ViewMatchController', function($scope, $routeParams, $location){
         $location.path('ViewTournament' + '/' + $scope.tournamentName);
     };
 });
+
+function updateTime(){
+    var schedule = new Date(2016, 4, 22).getTime();
+    return new Date(schedule-1000);
+}
